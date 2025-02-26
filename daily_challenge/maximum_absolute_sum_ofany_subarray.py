79f742c20f818b1ed974690fa9ad5cd762196028
rakshitha-kolla <krakshitha0987@gmail.com>
2025-02-26 22:26:17 +0530

    leetcode day 23

---
+++ daily_challenge/maximum_absolute_sum_ofany_subarray.py
@@ -0,0 +1,39 @@
+'''You are given an integer array nums. The absolute sum of a subarray [numsl, numsl+1, ..., numsr-1, numsr] is abs(numsl + numsl+1 + ... + numsr-1 + numsr).
+
+Return the maximum absolute sum of any (possibly empty) subarray of nums.
+
+Note that abs(x) is defined as follows:
+
+    If x is a negative integer, then abs(x) = -x.
+    If x is a non-negative integer, then abs(x) = x.
+
+
+
+Example 1:
+
+Input: nums = [1,-3,2,3,-4]
+Output: 5
+Explanation: The subarray [2,3] has absolute sum = abs(2+3) = abs(5) = 5.
+
+Example 2:
+
+Input: nums = [2,-5,1,-4,3,-2]
+Output: 8
+Explanation: The subarray [-5,1,-4] has absolute sum = abs(-5+1-4) = abs(-8) = 8.
+'''
+class Solution:
+    def maxAbsoluteSum(self, nums):
+        min_prefix_sum = 0
+        max_prefix_sum = 0
+        prefix_sum = 0
+
+        for num in nums:
+            prefix_sum += num
+
+            min_prefix_sum = min(min_prefix_sum, prefix_sum)
+            max_prefix_sum = max(max_prefix_sum, prefix_sum)
+
+        return max_prefix_sum - min_prefix_sum
+if __name__=="__main__":
+    s=Solution()
+    print(s.maxAbsoluteSum([1,-3,2,3,-4]))
