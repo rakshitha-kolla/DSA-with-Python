fc7273bf86350de239900558043d2ecf36a6bdb4
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-04 22:55:20 +0530

    leetcode day 60

---
+++ daily_challenge/lowest_common_ancestorsOf_deepest_leaves.py
@@ -0,0 +1,24 @@
+from typing import Optional
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def lcaDeepestLeaves(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        def dfs(root):
+            if not root:
+                return 0, None
+
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            if left[0] > right[0]:
+                return left[0] + 1, left[1]
+            if left[0] < right[0]:
+                return right[0] + 1, right[1]
+            return left[0] + 1, root
+
+        return dfs(root)[1]
+s=Solution()
+print(s.lcaDeepestLeaves([3,5,1,6,2,0,8,None,None,7,4]))
