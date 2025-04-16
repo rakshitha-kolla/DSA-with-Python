cbf529826ecc15198e257443f2234aa37fa29e03
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-16 23:53:22 +0530

    leetcode day 72

---
+++ daily_challenge/count_good_subarrays.py
@@ -0,0 +1,20 @@
+from collections import Counter
+
+class Solution:
+    def countGood(self, nums: list[int], k: int) -> int:
+        n = len(nums)
+        same, right = 0, -1
+        cnt = Counter()
+        ans = 0
+        for left in range(n):
+            while same < k and right + 1 < n:
+                right += 1
+                same += cnt[nums[right]]
+                cnt[nums[right]] += 1
+            if same >= k:
+                ans += n - right
+            cnt[nums[left]] -= 1
+            same -= cnt[nums[left]]
+        return ans
+s=Solution()
+print(s.countGood( [1,1,1,1,1],10))
