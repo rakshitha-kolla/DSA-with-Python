594ab2acb159a75eec97249d2caa53db64a77efb
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-20 11:59:20 +0530

    leetcode day 76

---
+++ daily_challenge/rabbits_in_forest.py
@@ -0,0 +1,11 @@
+from typing import Counter
+from math import ceil
+class Solution(object):
+    def numRabbits(self, answers):
+        mpp = Counter(answers)
+        total = 0
+        for x in mpp:
+            total += ceil(float(mpp[x]) / (x + 1)) * (x + 1)
+        return int(total)
+s=Solution()
+print(s.numRabbits([1,1,2]))
