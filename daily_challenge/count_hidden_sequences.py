4cb76fb590976a82a8e909220cf11e5622f962c7
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-21 10:15:40 +0530

    leetcode day 77

---
+++ daily_challenge/count_hidden_sequences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numberOfArrays(
+        self, differences: list[int], lower: int, upper: int
+    ) -> int:
+        x = y = cur = 0
+        for d in differences:
+            cur += d
+            x = min(x, cur)
+            y = max(y, cur)
+            if y - x > upper - lower:
+                return 0
+        return (upper - lower) - (y - x) + 1
+s=Solution()
+print(s.numberOfArrays([1,-3,4],1,6))
