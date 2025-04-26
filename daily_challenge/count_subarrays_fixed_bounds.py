9e50e6692106c7c0f1cf191f664980525c0e808c
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-26 21:12:55 +0530

    leetcode day 82

---
+++ daily_challenge/count_subarrays_fixed_bounds.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def countSubarrays(self, nums, minK, maxK):
+        count = 0
+        start = -1
+        mini = -1
+        maxi = -1
+        for i in range(len(nums)):
+            if nums[i] < minK or nums[i] > maxK:
+                start = i
+            if nums[i] == maxK:
+                maxi = i
+            if nums[i] == minK:
+                mini = i
+            valid = max(0, min(mini, maxi) - start)
+            count += valid
+        return count
+s=Solution()
+print(s.countSubarrays([1,3,5,2,7,5],1,5))
