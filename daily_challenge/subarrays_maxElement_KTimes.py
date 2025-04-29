2dd346adba41e093ea9c827dee898b3393e8172f
rakshitha-kolla <krakshitha0987@gmail.com>
2025-04-29 21:31:25 +0530

    leetcode day 85

---
+++ daily_challenge/subarrays_maxElement_KTimes.py
@@ -0,0 +1,18 @@
+class Solution:
+    def countSubarrays(self, nums: list[int], k: int) -> int:
+        max_element = max(nums)
+        indexes_of_max_elements = []
+        ans = 0
+
+        for index, element in enumerate(nums):
+
+            if element == max_element:
+                indexes_of_max_elements.append(index)
+
+            freq = len(indexes_of_max_elements)
+            if freq >= k:
+                ans += indexes_of_max_elements[-k] + 1
+
+        return ans
+s=Solution()
+print(s.countSubarrays([1,3,2,3,3],2))
