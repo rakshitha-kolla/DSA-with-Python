d54e9433b273c1aaf814667946bf6a2cfdb905c3
rakshitha-kolla <krakshitha0987@gmail.com>
2025-05-07 00:46:56 +0530

    leetcode day 92

---
+++ daily_challenge/build_array_permutation.py
@@ -0,0 +1,12 @@
+class Solution:
+    def buildArray(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        # Build the final value on the first iteration
+        for i in range(n):
+            nums[i] += 1000 * (nums[nums[i]] % 1000)
+        # Modified to final value on the second iteration
+        for i in range(n):
+            nums[i] //= 1000
+        return nums
+s=Solution()
+print(s.buildArray([0,2,1,5,3,4]))
