ee0f949b572836ca4b96868119d3525600facf59
rakshitha-kolla <krakshitha0987@gmail.com>
2025-05-20 18:46:48 +0530

    leetcode day 106

---
+++ daily_challenge/zero_array_transformation.py
@@ -0,0 +1,19 @@
+from typing import List
+class Solution:
+    def isZeroArray(self, nums: List[int], queries: List[List[int]]) -> bool:
+        deltaArray = [0] * (len(nums) + 1)
+        for left, right in queries:
+            deltaArray[left] += 1
+            deltaArray[right + 1] -= 1
+        operationCounts = []
+        currentOperations = 0
+        for delta in deltaArray:
+            currentOperations += delta
+            operationCounts.append(currentOperations)
+        for operations, target in zip(operationCounts, nums):
+            if operations < target:
+                return False
+        return True
+if __name__=="__main__":
+    s=Solution()
+    print(s.isZeroArray([1,0,1], [[0,2]]))
