0585b72996273c40de289d8e3ed549bf73ef4d2d
rakshitha-kolla <krakshitha0987@gmail.com>
2025-06-27 22:01:57 +0530

    leetcode day 144

---
+++ daily_challenge/longest_subsequence_repeatedK_times.py
@@ -0,0 +1,22 @@
+from collections import Counter
+from collections import deque
+class Solution:
+    def longestSubsequenceRepeatedK(self, s: str, k: int) -> str:
+        ans = ""
+        candidate = sorted(
+            [c for c, w in Counter(s).items() if w >= k], reverse=True
+        )
+        q = deque(candidate)
+        while q:
+            curr = q.popleft()
+            if len(curr) > len(ans):
+                ans = curr
+            # generate the next candidate string
+            for ch in candidate:
+                nxt = curr + ch
+                it = iter(s)
+                if all(ch in it for ch in nxt * k):
+                    q.append(nxt)
+        return ans
+s=Solution()
+print(s.longestSubsequenceRepeatedK( "letsleetcode", 2))
