52eebab630551f0d954df6667ac5179c17283ea3
rakshitha-kolla <krakshitha0987@gmail.com>
2025-07-09 20:55:33 +0530

    leetcode day 156

---
+++ daily_challenge/reschedule_meetings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxFreeTime(
+        self, eventTime: int, k: int, startTime: list[int], endTime: list[int]
+    ) -> int:
+        n = len(startTime)
+        res = 0
+        total = [0] * (n + 1)
+        for i in range(n):
+            total[i + 1] = total[i] + endTime[i] - startTime[i]
+        for i in range(k - 1, n):
+            right = eventTime if i == n - 1 else startTime[i + 1]
+            left = 0 if i == k - 1 else endTime[i - k]
+            res = max(res, right - left - (total[i + 1] - total[i - k + 1]))
+        return res
+s=Solution()
+print(s.maxFreeTime( 10,  1,  [0,2,9], [1,4,10]))
