3b3ea3c9963f34485727db1f70fc3adc72efbf30
rakshitha-kolla <krakshitha0987@gmail.com>
2025-09-02 15:27:19 +0530

    4 sum

---
+++ arrays/4sum.py
@@ -0,0 +1,31 @@
+from typing import List
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        n=len(nums)
+        for i in range(n):
+            if i>0 and nums[i]==nums[i-1]:
+                continue
+            for j in range(i+1,n):
+                if j>i+1 and nums[j]==nums[j-1]:
+                    continue
+                k=j+1
+                l=n-1
+                while(k<l):
+                    s=nums[i]+nums[j]+nums[k]+nums[l]
+                    if s<target:
+                        k+=1
+                    elif s>target:
+                        l-=1
+                    else:
+                        res.append([nums[i],nums[j],nums[k],nums[l]])
+                        while(k<l and nums[k]==nums[k+1]):
+                            k+=1
+                        while(k<l and nums[l]==nums[l-1]):
+                            l-=1
+                        l-=1
+                        k+=1
+        return res
+s=Solution()
+print(s.fourSum([1,0,-1,0,-2,2],0))
