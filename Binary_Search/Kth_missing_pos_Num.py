9978ba037e4e9ff8c01d3511bf00b76465855b47
rakshitha-kolla <krakshitha0987@gmail.com>
2025-10-26 00:20:01 +0530

    found Kth missing positive number

---
+++ Binary_Search/Kth_missing_pos_Num.py
@@ -0,0 +1,15 @@
+from typing import List
+class Solution:
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        low=0
+        high=len(arr)-1
+        while(low<=high):
+            mid=(low+high)//2
+            missing=arr[mid]-(mid+1)
+            if missing < k:
+                low=mid+1
+            else:
+                high=mid-1
+        return high+1+k
+s=Solution()    
+print(s.findKthPositive([2,3,4,7,11],5))
