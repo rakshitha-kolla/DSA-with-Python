f37298cea3831a606225dd95c5f07acf01cd1e54
rakshitha-kolla <krakshitha0987@gmail.com>
2025-12-20 17:58:28 +0530

    sum of odd primes

---
+++ Basic/sum_of_odd_primes.py
@@ -0,0 +1,19 @@
+
+# sum of odd prime numbers less than or equal to n
+class Solution:
+	def prime_Sum(self, n):
+		prime=[True]*(n+1)
+		prime[0]=prime[1]=False
+		for i in range(2,int(n**0.5)+1):
+		    if prime[i]:
+		        for j in range(i*i,n+1,i):
+		            prime[j]=False
+		sum_=0
+		for i in range(len(prime)):
+		    if prime[i] and i%2 !=0:
+		        sum_+=i
+		return sum_
+s=Solution()
+n=int(input())
+print(s.prime_Sum(n))   
+	
