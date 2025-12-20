cd4df5e3c5c2cd03a0be27f43cab3c49397b963f
rakshitha-kolla <krakshitha0987@gmail.com>
2025-12-20 19:49:07 +0530

    3sum

--- arrays/3sum.py
+++ arrays/3sum.py
@@ -7,21 +7,22 @@ class Solution:
         for i in range(n):
             if i>0 and nums[i]==nums[i-1]:
                 continue
-            left,right=i+1,n-1
-            while(left<right):
-                s=nums[i]+nums[left]+nums[right]
-                if s<0:
-                    left+=1
+            j=i+1
+            k=n-1
+            while(j<k):
+                s=nums[i]+nums[k]+nums[j]
+                if s < 0:
+                    j+=1
                 elif s>0:
-                    right-=1
+                    k-=1
                 else:
-                    res.append([nums[i],nums[left],nums[right]])
-                    while(left<right and nums[left]==nums[left+1]):
-                        left+=1
-                    while(left<right and nums[right]==nums[right-1]):
-                        right-=1
-                    left+=1
-                    right-=1
+                    res.append([nums[i],nums[j],nums[k]])
+                    j+=1
+                    k-=1
+                    while(j<k and nums[j]==nums[j-1]):
+                        j+=1
+                    while(j<k and nums[k]==nums[k+1]):
+                        k-=1
         return res
 s=Solution()
 print(s.threeSum([-1,0,1,2,-1,-4]))
