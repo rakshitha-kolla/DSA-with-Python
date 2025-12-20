b6b963c0e2d1de7f6a2eb0564ea0cc6d20a71f9a
rakshitha-kolla <krakshitha0987@gmail.com>
2025-12-20 19:19:21 +0530

    2sum

--- arrays/two_sum.py
+++ arrays/two_sum.py
@@ -1,11 +1,13 @@
+#   two sum
+from typing import List
+from collections import defaultdict
 class Solution:
-    def twoSum(self, nums: list[int], target: int) -> list[int]:
-        hash={}
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        d=defaultdict(int)
         for i in range(len(nums)):
-            complement=target-nums[i]
-            if complement in hash:
-                return [hash[complement],i]
-            hash[nums[i]]=i
-if __name__=="__main__":
-    s=Solution()
-    print(s.twoSum([2,7,11,15],9))
+            if target-nums[i] in d:
+                return [d[target-nums[i]],i]
+            d[nums[i]]=i
+        return []
+s=Solution()
+print(s.twoSum([2,7,11,15],9))
